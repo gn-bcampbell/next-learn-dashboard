04945c161e9a843ac3d385772e9feb79ceb845ac
Brendan <b.campbell787@gmail.com>
2024-09-02 12:34:38 +0100

    Chapter 7: Data fetching from PostgreSQL into server components

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -60,6 +60,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+    // Avoid waterfall data fetching by using Promise.all() to make parallel Promises.
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
