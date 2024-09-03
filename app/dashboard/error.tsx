80e4a2dc7ebbc07bb9dc3d57360bcdfd0c1e82a9
Brendan <b.campbell787@gmail.com>
2024-09-03 14:04:43 +0100

    Chapter 13: Error handling with default error.tsx page

---
+++ app/dashboard/error.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useEffect } from 'react';
+
+/*
+    Catch-all for unexpected errors and allows you to display a fallback UI to users.
+
+    - Needs to be a 'use client' client component
+    - Prop 1: JavaScript's native 'Error' object
+    - Prop 2: reset function that tries to re-render the route segment
+ */
+export default function Error({
+                                  error,
+                                  reset,
+                              }: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    useEffect(() => {
+        // Optionally log the error to an error reporting service
+        console.error(error);
+    }, [error]);
+
+    return (
+        <main className="flex h-full flex-col items-center justify-center">
+            <h2 className="text-center">Something went wrong!</h2>
+            <button
+                className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+                onClick={
+                    // Attempt to recover by trying to re-render the invoices route
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </main>
+    );
+}
