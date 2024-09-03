0bc71eb98af5b66e95caa728d8b8beea184351d5
Brendan <b.campbell787@gmail.com>
2024-09-03 12:25:37 +0100

    Chapter 12: Server Actions: updating an invoice
    note use of .bind() in edit-form

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -13,10 +13,11 @@ export function CreateInvoice() {
   );
 }
 
+// use template literal to create a dynamic route
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
