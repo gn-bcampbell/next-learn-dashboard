aef92990aabf4fc6c18f7804c991e52bac542b80
Brendan <b.campbell787@gmail.com>
2024-09-03 16:00:05 +0100

    Chapter 16: Metadata (actually useful)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import '@/app/ui/global.css'
 import {inter} from "@/app/ui/fonts";
 
+import { Metadata } from 'next';
+
+// template allows other pages to prefix their title eg `Invoices | Acme Dashboard`
+// https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function RootLayout({
   children,
 }: {
